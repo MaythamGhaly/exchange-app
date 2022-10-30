c4a1b475789fe195a98a258705f3313c0b8831ce
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-30 15:41:13 +0200

    frontend: add appbar to login page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -9,7 +9,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          shadowColor: const Color.fromARGB(255, 111, 8, 143),
+          elevation: 10,
+          title: const Text(
+            'ExpChange',
+            style: TextStyle(
+              color: Color.fromARGB(255, 47, 211, 233),
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
