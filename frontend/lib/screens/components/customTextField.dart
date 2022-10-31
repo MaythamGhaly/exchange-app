0b3dad3847524ab83bd47da7390b9174c041a24c
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-31 17:08:08 +0200

    frontend: add Custom TextField component

---
+++ frontend/lib/screens/components/customTextField.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String labelText;
+  final String hintText;
+  final bool isPassword;
+
+  const CustomTextField(
+      {required this.labelText,
+      required this.hintText,
+      this.isPassword = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        // ignore: prefer_const_literals_to_create_immutables
+        boxShadow: [
+          const BoxShadow(
+            color: Color.fromARGB(200, 92, 225, 230),
+            blurRadius: 15,
+            offset: Offset(-2, 4),
+          ),
+        ],
+      ),
+      child: TextField(
+        obscureText: isPassword,
+        decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25.0),
+              borderSide: const BorderSide(
+                  width: 1, color: Color.fromARGB(200, 92, 225, 230)),
+            ),
+            filled: true,
+            fillColor: const Color.fromARGB(255, 238, 238, 238),
+            labelText: labelText,
+            hintText: hintText,
+            hintStyle: const TextStyle(
+              color: Color.fromARGB(200, 92, 225, 230),
+            ),
+            labelStyle: const TextStyle(
+              color: Color.fromARGB(200, 92, 225, 230),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25.0),
+              borderSide: const BorderSide(
+                  width: 1, color: Color.fromARGB(200, 92, 225, 230)),
+            )),
+      ),
+    );
+  }
+}
