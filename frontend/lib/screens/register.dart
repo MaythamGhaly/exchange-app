07e824f171f4124006ea8968f4e2649766bc1a61
Maytham Ghaly <maythamghaly@gmail.com>
2022-11-01 23:47:17 +0200

    frontend: fix register page

--- frontend/lib/screens/register.dart
+++ frontend/lib/screens/register.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:drop_shadow_image/drop_shadow_image.dart';
+import 'package:frontend/screens/components/customTextForm.dart';
 
 import 'components/customButton.dart';
-import 'components/customTextField.dart';
-import 'components/getUserLocation.dart';
+import 'components/smallTextField.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -13,6 +13,13 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  TextEditingController _firstNameController = TextEditingController();
+  TextEditingController _lastNameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _confirmPasswordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
   hello() {
     print('hello');
   }
@@ -35,255 +42,91 @@ class _RegisterState extends State<Register> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 150.0,
-              child: Center(
-                child: DropShadowImage(
-                  offset: const Offset(5, 5),
-                  scale: 1,
-                  blurRadius: 20,
-                  image: Image.asset(
-                    'assets/Exchange-logo.png',
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 150.0,
+                child: Center(
+                  child: DropShadowImage(
+                    offset: const Offset(5, 5),
+                    scale: 1,
+                    blurRadius: 20,
+                    image: Image.asset(
+                      'assets/Exchange-logo.png',
+                    ),
                   ),
                 ),
               ),
-            ),
-            const Text(
-              'Register',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
-                  color: Color.fromARGB(200, 92, 225, 230),
-                  shadows: [
-                    Shadow(
-                      blurRadius: 10.0,
-                      color: Color.fromARGB(200, 122, 118, 128),
-                      offset: Offset(0, 4.0),
-                    ),
-                  ]),
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Container(
-                          margin: const EdgeInsets.only(
-                              left: 20, right: 10, top: 20),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(100),
-                            // ignore: prefer_const_literals_to_create_immutables
-                            boxShadow: [
-                              const BoxShadow(
-                                color: Color.fromARGB(200, 92, 225, 230),
-                                blurRadius: 15,
-                                offset: Offset(-2, 4),
-                              ),
-                            ],
-                          ),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(25.0),
-                                  borderSide: const BorderSide(
-                                      width: 1,
-                                      color: Color.fromARGB(200, 92, 225, 230)),
-                                ),
-                                filled: true,
-                                fillColor:
-                                    const Color.fromARGB(255, 238, 238, 238),
-                                labelText: 'First Name',
-                                hintText: ('Enter your first name'),
-                                hintStyle: const TextStyle(
-                                  color: Color.fromARGB(200, 92, 225, 230),
-                                ),
-                                labelStyle: const TextStyle(
-                                  color: Color.fromARGB(200, 92, 225, 230),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(25.0),
-                                  borderSide: const BorderSide(
-                                      width: 1,
-                                      color: Color.fromARGB(200, 92, 225, 230)),
-                                )),
-                          ),
-                        ),
+              const Text(
+                'Register',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    color: Color.fromARGB(200, 92, 225, 230),
+                    shadows: [
+                      Shadow(
+                        blurRadius: 10.0,
+                        color: Color.fromARGB(200, 122, 118, 128),
+                        offset: Offset(0, 4.0),
                       ),
-                      Expanded(
-                        child: Container(
-                          margin: const EdgeInsets.only(
-                              left: 10, right: 20, top: 20),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(100),
-                            // ignore: prefer_const_literals_to_create_immutables
-                            boxShadow: [
-                              const BoxShadow(
-                                color: Color.fromARGB(200, 92, 225, 230),
-                                blurRadius: 15,
-                                offset: Offset(-2, 4),
-                              ),
-                            ],
-                          ),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(25.0),
-                                  borderSide: const BorderSide(
-                                      width: 1,
-                                      color: Color.fromARGB(200, 92, 225, 230)),
-                                ),
-                                filled: true,
-                                fillColor:
-                                    const Color.fromARGB(255, 238, 238, 238),
-                                labelText: 'Last Name',
-                                hintText: ('Enter your last name'),
-                                hintStyle: const TextStyle(
-                                  color: Color.fromARGB(200, 92, 225, 230),
-                                ),
-                                labelStyle: const TextStyle(
-                                  color: Color.fromARGB(200, 92, 225, 230),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(25.0),
-                                  borderSide: const BorderSide(
-                                      width: 1,
-                                      color: Color.fromARGB(200, 92, 225, 230)),
-                                )),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ]),
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: SmallTextForm.customText(
+                      context,
+                      'First Name',
+                      'enter your first name',
+                      _firstNameController,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Container(
-              margin: const EdgeInsets.only(right: 20, left: 20, top: 20.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(100),
-                // ignore: prefer_const_literals_to_create_immutables
-                boxShadow: [
-                  const BoxShadow(
-                    color: Color.fromARGB(200, 92, 225, 230),
-                    blurRadius: 15,
-                    offset: Offset(-2, 4),
+                  Expanded(
+                    child: SmallTextForm.customText(
+                      context,
+                      'Last Name',
+                      'enter your last name',
+                      _lastNameController,
+                    ),
                   ),
                 ],
               ),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide: const BorderSide(
-                          width: 1, color: Color.fromARGB(200, 92, 225, 230)),
-                    ),
-                    filled: true,
-                    fillColor: const Color.fromARGB(255, 238, 238, 238),
-                    labelText: "Email",
-                    hintText: "Enter your email",
-                    hintStyle: const TextStyle(
-                      color: Color.fromARGB(200, 92, 225, 230),
-                    ),
-                    labelStyle: const TextStyle(
-                      color: Color.fromARGB(200, 92, 225, 230),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide: const BorderSide(
-                          width: 1, color: Color.fromARGB(200, 92, 225, 230)),
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(right: 20, left: 20, top: 20.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(100),
-                // ignore: prefer_const_literals_to_create_immutables
-                boxShadow: [
-                  const BoxShadow(
-                    color: Color.fromARGB(200, 92, 225, 230),
-                    blurRadius: 15,
-                    offset: Offset(-2, 4),
-                  ),
-                ],
-              ),
-              child: TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide: const BorderSide(
-                          width: 1, color: Color.fromARGB(200, 92, 225, 230)),
-                    ),
-                    filled: true,
-                    fillColor: const Color.fromARGB(255, 238, 238, 238),
-                    labelText: "Password",
-                    hintText: "Enter your password",
-                    hintStyle: const TextStyle(
-                      color: Color.fromARGB(200, 92, 225, 230),
-                    ),
-                    labelStyle: const TextStyle(
-                      color: Color.fromARGB(200, 92, 225, 230),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide: const BorderSide(
-                          width: 1, color: Color.fromARGB(200, 92, 225, 230)),
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(right: 20, left: 20, top: 20.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(100),
-                // ignore: prefer_const_literals_to_create_immutables
-                boxShadow: [
-                  const BoxShadow(
-                    color: Color.fromARGB(200, 92, 225, 230),
-                    blurRadius: 15,
-                    offset: Offset(-2, 4),
-                  ),
-                ],
-              ),
-              child: TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide: const BorderSide(
-                          width: 1, color: Color.fromARGB(200, 92, 225, 230)),
-                    ),
-                    filled: true,
-                    fillColor: const Color.fromARGB(255, 238, 238, 238),
-                    labelText: "Confirm Password",
-                    hintText: "Enter your password",
-                    hintStyle: const TextStyle(
-                      color: Color.fromARGB(200, 92, 225, 230),
-                    ),
-                    labelStyle: const TextStyle(
-                      color: Color.fromARGB(200, 92, 225, 230),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide: const BorderSide(
-                          width: 1, color: Color.fromARGB(200, 92, 225, 230)),
-                    )),
-              ),
-            ),
-            const LocationPage(),
-            CustomButton(
-              inputText: 'Register',
-              onpressed: () {
-                Navigator.push(
+              CustomTextForm.customText(context, 'Email', 'Enter your email',
+                  _emailController, false, _passwordController, false),
+              CustomTextForm.customText(
                   context,
-                  MaterialPageRoute(builder: (context) => const Register()),
-                );
-              },
-            ),
-          ],
+                  'Password',
+                  'Enter your password',
+                  _passwordController,
+                  true,
+                  _passwordController,
+                  true),
+              CustomTextForm.customText(
+                  context,
+                  'Confirm Password',
+                  'Enter your password',
+                  _confirmPasswordController,
+                  true,
+                  _passwordController,
+                  true),
+              CustomButton(
+                inputText: 'Register',
+                onpressed: () {
+                  // if (_formKey.currentState!.validate()) {
+                  //   ScaffoldMessenger.of(context).showSnackBar(
+                  //     const SnackBar(content: Text('yes')),
+                  //   );
+                  // } else {
+                  //   ScaffoldMessenger.of(context).showSnackBar(
+                  //     const SnackBar(content: Text('no')),
+                  //   );
+                  // }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
