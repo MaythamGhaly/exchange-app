4282366a175723698da917c4edcd3b4277b1f46a
Maytham Ghaly <maythamghaly@gmail.com>
2022-11-07 19:57:50 +0200

    backend: add routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,20 @@
 const {Router} = require('express');
 const authMiddleware = require('../middlewares/auth.middleware')
 const router = Router();
-const {addProduct, editProfile} = require('../controllers/user.controller')
+const {addProduct, editProfile, getAllProduct, getProductByCategory, addFavorite, getFavorites} = require('../controllers/user.controller')
 
 
 
 router.post('/add-Product',authMiddleware, addProduct);
 router.post('/edit-Profile',authMiddleware, editProfile);
+router.get('/get-all-products',authMiddleware, getAllProduct);
+router.post('/get-products-by-category',authMiddleware, getProductByCategory);
+router.post('/add-to-Favorite',authMiddleware, addFavorite);
+router.get('/get-user',authMiddleware, getFavorites);
+
+
+
+
 
 
 module.exports = router;
