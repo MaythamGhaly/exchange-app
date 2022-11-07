69aac7aab7c4f47bb671963639afc9b5d8ad6194
Maytham Ghaly <maythamghaly@gmail.com>
2022-11-07 14:05:43 +0200

    backend: add product model

---
+++ backend/models/products.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+
+    product_name: {
+        type: String,
+        required: 'product_name is required'
+    },
+    expiry_date: {
+        type: Date,
+        required: 'expiry_date is required'
+    },
+    picture_url: {
+        type: String,
+        required: 'picture_url is required'
+    },
+    description: {
+        type: String,
+        required: 'description is required'
+    },
+    category: {
+        type: String,
+        required: 'category is required',
+        enum: ['food', 'beverage', 'medicine', 'beauty', 'donate']
+    },
+    approved: {
+        type: Boolean,
+        default: false,
+    },
+    user :{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+})
+
+
+module.exports = mongoose.model('Product',productSchema);
