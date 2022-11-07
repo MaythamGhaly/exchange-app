2c5988d96298fcefce8af85678bf386a2af34ea1
Maytham Ghaly <maythamghaly@gmail.com>
2022-11-07 18:18:17 +0200

    backend: add product api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,26 +1,43 @@
 const User = require('../models/users.model');
+const Product = require('../models/products.model');
+
 
 
 const addProduct = async (req, res) => {
     id = req.user._id;
     const { product_name, expiry_date, picture_url, description , category  } = req.body;
-    const post = await Post.create({
+    const product = await Product.create({
         product_name,
         expiry_date,
         picture_url,
         description,
         category,
+        user: id
     });
-    await post.save();
+    await product.save();
 
     const userById = await User.findById(id);
 
-    userById.posts.push(post);
+    userById.products.push(product);
     await userById.save();
 
     return res.send(userById);
 }
 
+const editProfile = async (req, res) => {
+    id = req.user._id;
+    const { first_name, last_name, password , confirm_pass } = req.body;
+    const user = await User.findOneAndUpdate(id, {
+        first_name,
+        last_name,
+        password,
+        confirm_pass
+    })
+
+return res.send(user)
+}
+
 module.exports = {
-    addProduct
+    addProduct,
+    editProfile
 }
