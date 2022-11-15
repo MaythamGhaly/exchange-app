a027655f874955cff67e3ed3b8bc5093678e9da7
Maytham Ghaly <maythamghaly@gmail.com>
2022-11-15 16:50:28 +0200

    backend: connect to socket.io

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+var http = require('http');
 require('dotenv').config();
 require('./config/db.config')
 const app = express();
 app.use(express.json());
+var server = http.createServer(app);
+var io = require('socket.io')(server,{
+    cors: {
+        origin: "*", // allow to server to accept request from different origin
+    }
+});
 
 let cors = require("cors");
 app.use(cors())
@@ -19,7 +26,24 @@ app.use('/', AdminRoutes)
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT, (err)=>{
+const connectedUsers = new Set();
+io.on('connection', (socket) => {
+    console.log('Connected');
+    io.emit('connectedUsers', connectedUsers.size);
+    connectedUsers.add(socket.id);
+    socket.on('disconnect', () => {
+        console.log('Disconnected');
+        connectedUsers.delete(socket.id);
+    });
+    socket.on("message", (data) => {
+        console.log(data);
+        socket.broadcast.emit("message-receive", data);
+        
+    })
+});
+
+
+server.listen(process.env.PORT, (err)=>{
     if(err) throw err;
     console.log(`server running on port ${process.env.PORT}`);
 })
