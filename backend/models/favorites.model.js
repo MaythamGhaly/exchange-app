13434b5f3e5bae8f1b961a2533676dbd3e3b5fac
Maytham Ghaly <maythamghaly@gmail.com>
2022-11-18 04:31:48 +0200

    backend: add apis

--- backend/models/favorites.model.js
+++ backend/models/favorites.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // favoriteSchema
 const favoritesSchema = new mongoose.Schema({
 
-    product_id :{
+    product :{
         type:mongoose.Schema.Types.ObjectId,
         ref:'Product',
     },
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,17 +1,19 @@
 const { Router } = require('express');
 const authMiddleware = require('../middlewares/auth.middleware');
 const router = Router();
-const { addProduct, editProfile, getProductByCategory,addChat,getChat,getRooms, addFavorite, getFavorites,findOneOrCreat, getProductById, dealDone , upload, getUser } = require('../controllers/user.controller');
+const { addProduct, editProfile, getProductByCategory,addChat,getChat,getRooms, search, addFavorite,deleteFavorites, getFavorites,findOneOrCreat, getProductById, dealDone , upload, getUser } = require('../controllers/user.controller');
 // user routes
-router.post('/add-Product',upload.single('productPicture'), authMiddleware, addProduct);
-router.post('/edit-Profile', upload.single('profilePicture') , authMiddleware, editProfile);
+router.post('/add-Product',upload.single('imageFile'), authMiddleware, addProduct);
+router.post('/edit-Profile', upload.single('imageFile') , authMiddleware, editProfile);
 router.get('/get-products-by-category/:category', authMiddleware, getProductByCategory);
-router.post('/add-to-Favorite', authMiddleware, addFavorite);
+router.get('/add-to-Favorite/:product_id', authMiddleware, addFavorite);
+router.get('/search/:product_name', authMiddleware, search);
+router.get('/get-favorites', authMiddleware, getFavorites);
+router.get('/delete-from-Favorite/:product_id', authMiddleware, deleteFavorites);
 router.post('/find-create-room', authMiddleware, findOneOrCreat);
 router.post('/add-chat', authMiddleware, addChat);
 router.get('/get-chats/:id/:id', authMiddleware, getChat);
 router.get('/get-rooms', authMiddleware, getRooms);
-router.get('/get-favorites', authMiddleware, getFavorites);
 router.get('/get-product-by-id/:id', authMiddleware, getProductById);
 router.get('/deal-done/:id', authMiddleware, dealDone);
 router.get('/get-user', authMiddleware, getUser);
